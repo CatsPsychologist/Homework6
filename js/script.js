f3902c511b69c47c7a6ac6ad942b67b6a8829c60
CatsPsychologist <mainofficee@gmail.com>
2021-06-17 09:34:50 +0300

    upd

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let number = prompt('Введите Ваше число');
+let degree = prompt('Введите степень этого числа');
+
+
+function result (number, degree = 1) {
+
+
+    if (isNaN(number)){
+
+        alert('Type only numbers please')
+
+        return ;
+
+    }
+    if (isNaN(degree)){
+
+        alert('Type only numbers please')
+
+        return ;
+
+    }
+
+
+
+    return number ** degree;
+
+}
+
+
+let res = result(number,degree);
+
+alert(res);
